0176101278d711a414e7552840157cfa5f168d4b
Owen Mariani <mariani.owen@gmail.com>
2024-11-20 10:31:01 -0500

    wordle mostly complete

--- wordle/app.py
+++ wordle/app.py
@@ -14,9 +14,9 @@ class WordleGame:
         self.map = {}
         for i in range(len(self.word)):
             if self.word[i] in self.map:
-                self.map[self.word[i]] += i
+                self.map[self.word[i]] += 1
             else:
-                self.map[self.word[i]] = i
+                self.map[self.word[i]] = 1
     
     def __repr__(self):
         return f"Attempt #{self.attempt}/6!"
@@ -26,9 +26,55 @@ class WordleGame:
             for c in range(len(self.board[0])):
                 print(self.board[r][c], end=" ")
             print()
+    def play(self):
+        success = False
+        while self.attempt < 7 and not success:
+            success = self.make_guess()
+
+    def make_guess(self):
+        while(True):
+            guess = input("Enter your 5 letter guess: ")
+
+            if len(guess) != 5:
+                print("Guesses must be 5 letters long. Please try again")
+                continue
+
+            elif not guess.isalpha():
+                print("Only letters allowed. Please try again")
+                continue
+
+
+            guess = guess.lower()
+            
+
+            for i in range(self.attempt):
+                if "".join(self.board[i]) == guess:
+                    print("You've already guessed that word! Please try again")
+                    break
+            else:
+                print(f'Your guess: {guess}')
+
+                for j in range(5):
+                    if guess[j] == self.word[j]:
+                        self.board[self.attempt-1][j] = guess[j].upper()
+                    elif guess[j] in self.word:
+                         self.board[self.attempt-1][j] = guess[j]+ str(1)
+                    else:
+                        self.board[self.attempt-1][j] = guess[j]
+                
+                self.attempt +=1
+                self.display_board()
+                if guess == self.word:
+                    print("Congratulations, you've guessed the word!")
+                    return True
+                else:
+                    return False
+        
 
 if __name__ == "__main__":
     game = WordleGame()
     print(game)
     game.display_board()
+
+    game.play()
     
